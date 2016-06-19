5639f2216d71a473a2a8b83d33d919a50630fc3a
magl88 <magl88net@gmail.com>
2016-06-19 13:05:48 +0300

    Update Header

--- header.php
+++ header.php
@@ -14,9 +14,80 @@
 </head>
 <body>
 <div class="wrapper">
-	<div class=""></div>
 	<header>
-
+		<div class="container-fluid head-top-line">
+			<nav class=" container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-line"
+					        aria-expanded="false">
+						<span class="sr-only">Навигация</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+				<div class="collapse navbar-collapse" id="top-line">
+					<ul class="nav navbar-nav navbar-right nav-kab">
+						<li><a href="#" title="Авторизация">Авторизация</a></li>
+						<li><a href="#" title="Регистрация">Регистрация</a></li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right line-menu">
+						<li><a href="#" title="Отзывы">Отзывы</a></li>
+						<li><a href="#" title="О нас">О нас</a></li>
+						<li><a href="#" title="Блог">Блог <sup>2</sup></a></li>
+						<li><a href="#" title="Новости">Новости <sup>5</sup></a></li>
+						<li><a href="#" title="Команда">Команда</a></li>
+						<li><a href="#" title="Контакты">Контакты</a></li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</nav>
+		</div>
+		<div class="head container">
+			<div class="row">
+				<div class="col-lg-2">
+					<a href="/" class="logo" title="ibike.by"><img src="/img/logo.jpg" alt="ibike.by"></a>
+				</div>
+				<div class="col-lg-3">
+					<div class="block-search">
+						<form action="#" class="form-search">
+							<input type="text" class="field-search">
+							<input type="submit" class="field-send">
+						</form>
+					</div>
+					<div class="block-workTime">
+						Режим работы: с 11.00 до 20.00,<br>Пн - выходной
+					</div>
+				</div>
+				<div class="col-lg-5">
+					<div class="row">
+						<div class="col-lg-6">
+							<div class="block-phone">
+								<a href="tel:80447737374">8 (044)<span>773-73-74</span></a>
+								<a href="tel:80336373730">8 (033)<span>637-37-30</span></a>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="block-contact">
+								<a href="#map" class="adres">г.Минск, ул.Кузьмы Чорного 8</a>
+								<a href="mileto:bmxdelo@gmail.com" class="mail">bmxdelo@gmail.com</a>
+								<a href="skype:ibike.by?chat" class="skype">ibike.by</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-2">
+					<div class="dropdown block-miniCart">
+						<button class="btn btn-default dropdown-toggle" type="button" id="dropdownCart" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+							<span>Товаров: 0 шт</span>
+							<span>на сумму: 0 руб</span>
+						</button>
+						<div class="dropdown-menu" aria-labelledby="dropdownCart">
+							<span class="empty">Ваша корзина пуста</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</header>
 	<div class="content-wrapper">
 
