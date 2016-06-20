500f3568fac073d3d3e3c2a7760650213c35e7e9
magl88 <magl88net@gmail.com>
2016-06-20 13:05:03 +0300

    Update Font. Add Slider

--- index.php
+++ index.php
@@ -1,4 +1,53 @@
 <?php include "header.php" ?>
+<div class="container block-slide visible-md-block visible-lg-block">
+	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+			<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+			<li data-target="#carousel-example-generic" data-slide-to="3"></li>
+			<li data-target="#carousel-example-generic" data-slide-to="4"></li>
+		</ol>
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner" role="listbox">
+			<div class="item active">
+				<img src="/images/slide/slide-1.jpg" alt="slide">
+				<div class="carousel-caption">
+					<div style="color: #1e3071; font-size: 80px;">BMX</div>
+					<div style="color: #1e3071; font-size: 46px;">по вкусным<br>ценам</div>
+					<a href="#" class="btn-more">Подробнее</a>
+				</div>
+			</div>
+			<div class="item">
+				<img src="/images/slide/slide-2.jpg" alt="slide">
+				<div class="carousel-caption">
+					<div style="color: #1e3071; font-size: 80px;">BMX</div>
+					<div style="color: #1e3071; font-size: 46px;">по вкусным<br>ценам</div>
+					<a href="#" class="btn-more">Подробнее</a>
+				</div>
+			</div>
+			<div class="item">
+				<img src="/images/slide/slide-3.jpg" alt="slide">
+				<div class="carousel-caption">
+					<div style="color: #1e3071; font-size: 80px;">BMX</div>
+					<div style="color: #1e3071; font-size: 46px;">по вкусным<br>ценам</div>
+					<a href="#" class="btn-more">Подробнее</a>
+				</div>
+			</div>
+			<div class="item">
+				<img src="/images/slide/slide-4.jpg" alt="slide">
+				<div class="carousel-caption">
+					<div style="color: #1e3071; font-size: 80px;">BMX</div>
+					<div style="color: #1e3071; font-size: 46px;">по вкусным<br>ценам</div>
+					<a href="#" class="btn-more">Подробнее</a>
+				</div>
+			</div>
+		</div>
+		<!-- Controls -->
+		<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">		Предыдущий</a>
+		<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">			Следующий</a>
+	</div>
+</div>
 	<div class="container block-brend hidden-xs">
 		<div class="row">
 			<div class="col-xs-12">
