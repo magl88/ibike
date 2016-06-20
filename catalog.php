f1eb4c949e8445edf679d897e10848d051ffe174
magl88 <magl88net@gmail.com>
2016-06-20 23:24:54 +0300

    Update Catalog menu

--- catalog.php
+++ catalog.php
@@ -7,34 +7,130 @@
 		</ul>
 	</div>
 	<div class="container page-catalog">
-		<div class="col-lg-3">
-			<div class="">
-				<nav class="block-catalog-nav navmenu navmenu-default" role="navigation">
-					<a class="navmenu-brand" href="#">Название</a>
-					<ul class="nav navmenu-nav">
-						<li class="active"><a href="#">Пункт 1</a></li>
-						<li><a href="#">Пункт 2</a></li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Пункт 3 <b class="caret"></b></a>
-							<ul class="dropdown-menu navmenu-nav" role="menu">
-								<li><a href="#">Пункт 3.1</a></li>
-								<li><a href="#">Пункт 3.2</a>
-									<ul class="dropdown-menu navmenu-nav" role="menu">
-										<li><a href="#">Пункт 3.1</a></li>
-										<li><a href="#">Пункт 3.2</a></li>
-										<li><a href="#">Пункт 3.3</a></li>
-										<li><a href="#">Пункт 3.4</a></li>
-									</ul>
-								</li>
-								<li><a href="#">Пункт 3.3</a></li>
-								<li><a href="#">Пункт 3.4</a></li>
-							</ul>
-						</li>
-					</ul>
-				</nav>
+		<div class="col-lg-3 col-md-3">
+
+	<!-- ==============================================
+	<div id="MainMenu">
+				<div class="list-group panel">
+					<a href="#item3" class="list-group-item list-group-item-success" data-toggle="collapse" data-parent="#MainMenu">Трюковые самокаты</a>
+					<div class="collapse" id="item3">
+						<a class="list-group-item" href="">Самокаты</a>
+						<a class="list-group-item" href="">Запчасти</a>
+						<a class="list-group-item" href="">Колёса</a>
+						<a class="list-group-item" href="">Подшипники</a>
+						<a class="list-group-item" href="">Рули</a>
+						<a class="list-group-item" href="">Доски</a>
+					</div>
+					<a href="#item4" class="list-group-item list-group-item-success" data-toggle="collapse" data-parent="#MainMenu">Защита</a>
+					<div class="collapse" id="item4">
+						<a href="" class="list-group-item">Голень</a>
+						<a href="" class="list-group-item">Колено</a>
+						<a href="" class="list-group-item">Локоть</a>
+						<a href="" class="list-group-item">Перчатки</a>
+						<a href="" class="list-group-item">Шлем</a>
+						<a href="" class="list-group-item">Щиколотка</a>
+					</div>
+					<a href="#item5" class="list-group-item list-group-item-success" data-toggle="collapse" data-parent="#MainMenu">Аксессуары</a>
+					<div class="collapse" id="item5">
+						<a href="" class="list-group-item">Наклейки</a>
+						<a href="" class="list-group-item">Фонарики</a>
+						<a href="" class="list-group-item">Колонки</a>
+					</div>
+				</div>
 			</div>
+			============================================== -->
+
+			<nav class="block-catalog-nav navmenu navmenu-default" role="navigation">
+				<ul class="level-1">
+					<li class="active"><a href="#">Велосипеды</a>
+						<ul class="level-2">
+							<li class="active sub-level"><a href="#">Запчасти к велосипедам</a>
+								<ul class="level-3">
+									<li><a href="#">Звёзды</a></li>
+									<li class="active"><a href="#">Втулки</a></li>
+									<li><a href="#">Трещётки</a></li>
+									<li><a href="#">Рули</a></li>
+									<li><a href="#">Шины</a></li>
+								</ul>
+							</li>
+							<li class="sub-level"><a href="#" >Велосипеды в СБОРЕ</a>
+								<ul class="level-3 collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+									<li><a href="#">Горные</a></li>
+									<li><a href="#">Дорожные</a></li>
+									<li><a href="#">Подростковые</a></li>
+									<li><a href="#">Городские</a></li>
+									<li><a href="#">Туристические</a></li>
+									<li><a href="#">Шоссейные</a></li>
+									<li><a href="#">Детские</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+					<li><a href="#">BMX</a>
+						<ul class="level-2">
+							<li><a href="#">BMX в СБОРЕ</a></li>
+							<li class="sub-level"><a href="#">Запчасти BMX</a>
+								<ul class="level-3">
+									<li><a href="#">Баренды</a></li>
+									<li><a href="#">Вилки</a></li>
+									<li><a href="#">Втулки передние</a></li>
+									<li><a href="#">Втулки задние</a></li>
+									<li><a href="#">Выноса</a></li>
+									<li><a href="#">Грипсы</a></li>
+									<li><a href="#">Запчасти для втулок</a></li>
+									<li><a href="#">Звёзды </a></li>
+									<li><a href="#">Камеры и ободные ленты</a></li>
+									<li><a href="#">Каретки</a></li>
+									<li><a href="#">Колёса</a></li>
+									<li><a href="#">Обода</a></li>
+									<li><a href="#">Пеги</a></li>
+									<li><a href="#">Педали</a></li>
+									<li><a href="#">Подседельные зажимы</a></li>
+									<li><a href="#">Подседельные штыри</a></li>
+									<li><a href="#">Покрышки</a></li>
+									<li><a href="#">Рамы</a></li>
+									<li><a href="#">Рулевые</a></li>
+									<li><a href="#">Рули</a></li>
+									<li><a href="#">Сёдла</a></li>
+									<li><a href="#">Спицы</a></li>
+									<li><a href="#">Хабгарды</a></li>
+									<li><a href="#">Цепи</a></li>
+									<li><a href="#">Шатуны</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+					<li><a href="#">Трюковые самокаты</a>
+						<ul class="level-2">
+							<li><a href="#">Самокаты</a></li>
+							<li><a href="#">Запчасти</a></li>
+							<li><a href="#">Колёса</a></li>
+							<li><a href="#">Подшипники</a></li>
+							<li><a href="#">Рули</a></li>
+							<li><a href="#">Доски</a></li>
+						</ul>
+					</li>
+					<li><a href="#">Защита</a>
+						<ul class="level-2">
+							<li><a href="#">Голень</a></li>
+							<li><a href="#">Колено</a></li>
+							<li><a href="#">Локоть</a></li>
+							<li><a href="#">Перчатки</a></li>
+							<li><a href="#">Шлем</a></li>
+							<li><a href="#">Щиколотка</a></li>
+						</ul>
+					</li>
+					<li><a href="#">Аксессуары</a>
+						<ul class="level-2">
+							<li><a href="#">Наклейки</a></li>
+							<li><a href="#">Фонарики</a></li>
+							<li><a href="#">Колонки</a></li>
+						</ul>
+					</li>
+				</ul>
+			</nav>
 		</div>
-		<div class="col-lg-9 main-catalog">
+		<div class="col-lg-9 col-md-9 main-catalog">
 			<div class="row block-sort">
 				<div class="col-xs-12">
 					<div class="sort-by">
@@ -51,7 +147,7 @@
 				</div>
 				<div class="col-xs-12">
 					<div class="filter-by">
-					<span>Фильтр по товарам:</span>
+						<span>Фильтр по товарам:</span>
 						<ul>
 							<li><select name="select1" id="select1">
 									<option selected value="0">пол</option>
@@ -93,7 +189,7 @@
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-md-4 col-sm-6">
+				<div class="col-lg-4 col-md-6 col-sm-6">
 					<div class="item">
 						<span class="sale ilabel">скидка</span>
 						<div class="name"><a href="#">Набор ключей SALT PRO</a></div>
@@ -107,7 +203,7 @@
 						<div class="buy"><a href="#">Подробнее</a></div>
 					</div>
 				</div>
-				<div class="col-md-4 col-sm-6">
+				<div class="col-lg-4 col-md-6 col-sm-6">
 					<div class="item">
 						<div class="name"><a href="#">Bmx звезда United Metro</a></div>
 						<div class="img-wrap">
@@ -119,7 +215,7 @@
 						<div class="buy"><a href="#">Подробнее</a></div>
 					</div>
 				</div>
-				<div class="col-md-4 col-sm-6">
+				<div class="col-lg-4 col-md-6 col-sm-6">
 					<div class="item">
 						<span class="new ilabel">новинка</span>
 						<div class="name"><a href="#">BMX велосипед WeThePeople Curse </a></div>
@@ -132,7 +228,7 @@
 						<div class="buy"><a href="#">Подробнее</a></div>
 					</div>
 				</div>
-				<div class="col-md-4 col-sm-6">
+				<div class="col-lg-4 col-md-6 col-sm-6">
 					<div class="item">
 						<div class="name"><a href="#">BMX велосипед WeThePeople Curse </a></div>
 						<div class="img-wrap">
@@ -144,7 +240,7 @@
 						<div class="buy"><a href="#">Подробнее</a></div>
 					</div>
 				</div>
-				<div class="col-md-4 col-sm-6">
+				<div class="col-lg-4 col-md-6 col-sm-6">
 					<div class="item">
 						<div class="name"><a href="#">BMX втулка Eclat Dynamic.</a></div>
 						<div class="img-wrap">
@@ -156,7 +252,7 @@
 						<div class="buy"><a href="#">Подробнее</a></div>
 					</div>
 				</div>
-				<div class="col-md-4 col-sm-6">
+				<div class="col-lg-4 col-md-6 col-sm-6">
 					<div class="item">
 
 						<div class="name"><a href="#">BMX велосипед WeThePeople Curse</a></div>
@@ -169,7 +265,7 @@
 						<div class="buy"><a href="#">Подробнее</a></div>
 					</div>
 				</div>
-				<div class="col-md-4 col-sm-6">
+				<div class="col-lg-4 col-md-6 col-sm-6">
 					<div class="item">
 						<div class="name"><a href="#">Bmx звезда United Metro</a></div>
 						<div class="img-wrap">
@@ -181,7 +277,7 @@
 						<div class="buy"><a href="#">Подробнее</a></div>
 					</div>
 				</div>
-				<div class="col-md-4 col-sm-6">
+				<div class="col-lg-4 col-md-6 col-sm-6">
 					<div class="item">
 						<div class="name"><a href="#">BMX велосипед WeThePeople Curse</a></div>
 						<div class="img-wrap">
@@ -193,7 +289,7 @@
 						<div class="buy"><a href="#">Подробнее</a></div>
 					</div>
 				</div>
-				<div class="col-md-4 col-sm-6">
+				<div class="col-lg-4 col-md-6 col-sm-6">
 					<div class="item">
 						<div class="name"><a href="#">BMX втулка Eclat Dynamic.</a></div>
 						<div class="img-wrap">
@@ -205,7 +301,7 @@
 						<div class="buy"><a href="#">Подробнее</a></div>
 					</div>
 				</div>
-				<div class="col-md-4 col-sm-6">
+				<div class="col-lg-4 col-md-6 col-sm-6">
 					<div class="item">
 						<div class="name"><a href="#">Bmx звезда United Metro</a></div>
 						<div class="img-wrap">
@@ -217,7 +313,7 @@
 						<div class="buy"><a href="#">Подробнее</a></div>
 					</div>
 				</div>
-				<div class="col-md-4 col-sm-6">
+				<div class="col-lg-4 col-md-6 col-sm-6">
 					<div class="item">
 						<div class="name"><a href="#">BMX велосипед WeThePeople Curse</a></div>
 						<div class="img-wrap">
@@ -229,7 +325,7 @@
 						<div class="buy"><a href="#">Подробнее</a></div>
 					</div>
 				</div>
-				<div class="col-md-4 col-sm-6">
+				<div class="col-lg-4 col-md-6 col-sm-6">
 					<div class="item">
 						<div class="name"><a href="#">BMX вилка Eclat Stevie</a></div>
 						<div class="img-wrap">
@@ -249,7 +345,7 @@
 							<a href="#" class="btn-all">Все товары</a>
 						</div>
 						<div class="pager-wrap">
-							<ul >
+							<ul>
 								<li class="active"><span>1</span></li>
 								<li><a href="#">2</a></li>
 								<li><a href="#">3</a></li>
