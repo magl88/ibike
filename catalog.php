fc99cf2d0ee0527bb6440055d4374504cf9320ba
magl88 <magl88net@gmail.com>
2016-06-20 15:56:15 +0300

    Update Catalog

--- catalog.php
+++ catalog.php
@@ -8,9 +8,90 @@
 	</div>
 	<div class="container page-catalog">
 		<div class="col-lg-3">
-			Велосипеды
+			<div class="">
+				<nav class="block-catalog-nav navmenu navmenu-default" role="navigation">
+					<a class="navmenu-brand" href="#">Название</a>
+					<ul class="nav navmenu-nav">
+						<li class="active"><a href="#">Пункт 1</a></li>
+						<li><a href="#">Пункт 2</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Пункт 3 <b class="caret"></b></a>
+							<ul class="dropdown-menu navmenu-nav" role="menu">
+								<li><a href="#">Пункт 3.1</a></li>
+								<li><a href="#">Пункт 3.2</a>
+									<ul class="dropdown-menu navmenu-nav" role="menu">
+										<li><a href="#">Пункт 3.1</a></li>
+										<li><a href="#">Пункт 3.2</a></li>
+										<li><a href="#">Пункт 3.3</a></li>
+										<li><a href="#">Пункт 3.4</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Пункт 3.3</a></li>
+								<li><a href="#">Пункт 3.4</a></li>
+							</ul>
+						</li>
+					</ul>
+				</nav>
+			</div>
 		</div>
 		<div class="col-lg-9 main-catalog">
+			<div class="row block-sort">
+				<div class="col-xs-12">
+					<div class="sort-by">
+						<span>СортироВАТЬ ПО: </span>
+						<ul>
+							<li><a href="#">цене</a></li>
+							<li class="active"><a href="#">новизне</a></li>
+							<li><a href="#">названию</a></li>
+							<li><a href="#">скидке</a></li>
+							<li><a href="#">весу</a></li>
+						</ul>
+					</div>
+					<div class="page-by hidden-xs">Страница: 1 из 45</div>
+				</div>
+				<div class="col-xs-12">
+					<div class="filter-by">
+					<span>Фильтр по товарам:</span>
+						<ul>
+							<li><select name="select1" id="select1">
+									<option selected value="0">пол</option>
+									<option value="1">М</option>
+									<option value="2">Ж</option>
+								</select></li>
+							<li><select name="select2" id="select2">
+									<option selected value="0">бренд</option>
+									<option value="1">1</option>
+									<option value="2">2</option>
+								</select></li>
+							<li><select name="select3" id="select3">
+									<option selected value="0">цена</option>
+									<option value="1">1</option>
+									<option value="2">2</option>
+								</select></li>
+							<li><select name="select4" id="select4">
+									<option selected value="0">сезон</option>
+									<option value="1">1</option>
+									<option value="2">2</option>
+								</select></li>
+							<li><select name="select5" id="select5">
+									<option selected value="0">размер</option>
+									<option value="1">1</option>
+									<option value="2">2</option>
+								</select></li>
+							<li><select name="select6" id="select6">
+									<option selected value="0">цвет</option>
+									<option value="1">1</option>
+									<option value="2">2</option>
+								</select></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12">
+					<h1 class="title-page">Запчасти к велосипедам</h1>
+				</div>
+			</div>
 			<div class="row">
 				<div class="col-md-4 col-sm-6">
 					<div class="item">
@@ -158,6 +239,25 @@
 							<div class="price">Цена: 100 000 руб</div>
 						</div>
 						<div class="buy"><a href="#">Подробнее</a></div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="block-prod-pager">
+						<div class="btn-wrap">
+							<a href="#" class="btn-all">Все товары</a>
+						</div>
+						<div class="pager-wrap">
+							<ul >
+								<li class="active"><span>1</span></li>
+								<li><a href="#">2</a></li>
+								<li><a href="#">3</a></li>
+								<li><a href="#">4</a></li>
+								<li><span>...</span></li>
+								<li><a href="#">12</a></li>
+							</ul>
+						</div>
 					</div>
 				</div>
 			</div>
